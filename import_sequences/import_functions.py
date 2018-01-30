78599f14fcbde2604ceab1b0300581e3040940af
Jim Delproposto <delpropo@umich.edu>
2018-01-29 20:05:47 -0500

    Commit on 1-29-18

---
+++ import_sequences/import_functions.py
@@ -0,0 +1,15 @@
+from Bio import SeqIO
+
+def import_from_email(pasted_data:str):
+    #TODO:write function to parse data pasted from email
+    pass
+
+def import_from_abi(folder_name:str):
+    #TODO:write function to import all sequences from a folder
+    pass
+
+def import_oligos():
+    #TODO:write a function to import all oligo sequences for alignment.
+
+def import_DNA_seq():
+    #TODO:write a function that import DNA and or protein sequences for alignment with sequencing data
